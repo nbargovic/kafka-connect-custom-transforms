1bf1bac040f7e9374da645c3c4ed9f15593d38c9
Nick Bargovic <bargovic@gmail.com>
2024-11-14 16:17:29 -0500

    Update InsertTimestampTest.java

--- src/test/java/io/confluent/kafka/connect/transforms/InsertTimestampTest.java
+++ src/test/java/io/confluent/kafka/connect/transforms/InsertTimestampTest.java
@@ -79,4 +79,33 @@ public class InsertTimestampTest {
         assertNotNull(((Map) transformedRecord.value()).get("il5-timestamp"));
 
     }
+
+    @Test
+    public void copySchemaAndNullValue() {
+      final Map<String, Object> props = new HashMap<>();
+
+      props.put("ts.field.name", "il5-timestamp");
+
+      xform.configure(props);
+
+      final Schema simpleStructSchema = SchemaBuilder.struct().name("name").version(1).doc("doc").field("magic", Schema.OPTIONAL_INT64_SCHEMA).field("vars", Schema.STRING_SCHEMA).build();
+      final Struct simpleStruct = new Struct(simpleStructSchema).put("magic", 42L);
+
+      final SourceRecord record = new SourceRecord(null, null, "test", 0, simpleStructSchema, simpleStruct);
+      final SourceRecord transformedRecord = xform.apply(record);
+
+      assertEquals(simpleStructSchema.name(), transformedRecord.valueSchema().name());
+      assertEquals(simpleStructSchema.version(), transformedRecord.valueSchema().version());
+      assertEquals(simpleStructSchema.doc(), transformedRecord.valueSchema().doc());
+
+      assertEquals(Schema.OPTIONAL_INT64_SCHEMA, transformedRecord.valueSchema().field("magic").schema());
+      assertEquals(42L, ((Struct) transformedRecord.value()).getInt64("magic").longValue());
+      assertEquals(Schema.INT64_SCHEMA, transformedRecord.valueSchema().field("il5-timestamp").schema());
+      assertNotNull(((Struct) transformedRecord.value()).getInt64("il5-timestamp"));
+
+      // Exercise caching
+      final SourceRecord transformedRecord2 = xform.apply(
+        new SourceRecord(null, null, "test", 1, simpleStructSchema, new Struct(simpleStructSchema)));
+      assertSame(transformedRecord.valueSchema(), transformedRecord2.valueSchema());
+  }
 }
